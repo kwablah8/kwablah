1861bb6e023cbc9be26675fe9407c8a28ac0a465
kwablah8 <ernestkwablah19@gmail.com>
2023-05-03 21:18:48 +0000

    re-adjustments to homepage responsiveness

--- script/script.js
+++ script/script.js
@@ -5,9 +5,14 @@ arrowIcons = document.querySelectorAll(".wrapper i");
 let isDragStart = false, prevPageX, prevScrollLeft;
 let firstImgWidth = firstImg.clientWidth + 14;
 
+const showHideIcons = () => {
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+}
+
 arrowIcons.forEach(icon => {
     icon.addEventListener("click", () => {
         carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60);
     })
 });
 
